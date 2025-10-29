10ab64aef0cba332059be9fa060f268ccf2473f8
Adityaz <apple@Adityaz.local>
2025-10-29 13:37:04 +0530

    Started

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button className="text-2xl hover:cursor-pointer">Click Me!</Button>
+    </div>
+  );
+};
+
+export default Home;
