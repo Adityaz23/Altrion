535f4f3ea786f650a8154dab1ec61ca951910e23
Adityaz <apple@Adityaz.local>
2025-11-04 20:02:38 +0530

    Added the sign up and sign in only the database setup is left

---
+++ lib/action/auth.action.ts
@@ -0,0 +1,7 @@
+export async function signInWithEmail(email: string, password: string) {
+  // sign-in logic here
+}
+
+export async function signUpWithEmail(email: string, password: string) {
+  // sign-up logic here
+}
